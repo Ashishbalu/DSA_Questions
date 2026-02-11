5a0b2898b30beea45ad72371f608ac148291174f
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-02-11 21:18:23 +0530

    Added a new program of sorting

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,35 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    static void insertionSort(int[] arr, int n) {
+        for (int i = 0; i <= n-1 ; i++) {
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        insertionSort(arr, n);
+       for(int x : arr){
+           System.out.println(x);
+       }
+    }
+}
