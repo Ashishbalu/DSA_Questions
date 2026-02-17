8ced397572d9475ccf921a908251f7500b3dae03
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-02-17 21:17:49 +0530

    added new array program

--- src/array/SecondLargestElement.java
+++ src/array/SecondLargestElement.java
@@ -1,5 +1,19 @@
 package array;
 
 public class SecondLargestElement {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 7, 5, 7};
 
+        int n = arr.length;
+        int largest = arr[0];
+        int secLargest = -1;
+        for (int i = 0; i < n ; i++) {
+            if (arr[i] > largest){
+                largest = arr[i];
+            }else if (arr[i] < largest && arr[i] > secLargest){
+                secLargest = arr[i];
+            }
+        }
+        System.out.println("first largest element is: " + largest + ", Second Lagest is: " + secLargest);
+    }
 }
