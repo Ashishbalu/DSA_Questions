aadf8a93d8ba535a85ae7fc950f04639140d3beb
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-03-04 20:38:36 +0530

    added a new program to the array

---
+++ src/array/MaxConsecutiveOne.java
@@ -0,0 +1,23 @@
+package array;
+
+public class MaxConsecutiveOne {
+    // Time complexity -> O(n)
+    // Space complexity -> O(1)
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1, 0, 1, 1};
+        int n = arr.length;
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i <n ; i++) {
+            if (arr[i] == 1){
+                count++;
+                max = Math.max(max, count);
+            }else {
+                count = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
