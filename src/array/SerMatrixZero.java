2d1efb5d12f1c5d68f6ec6a2b8d561b904d3012b
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-06-23 21:51:31 +0530

    added a new problem to the Array

---
+++ src/array/SerMatrixZero.java
@@ -0,0 +1,42 @@
+package array;
+
+public class SerMatrixZero {
+        // this is an better approach
+    // TC --> O(m*n)
+    public void setZeros(int[][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for (int i = 0; i < m ; i++) {
+            for (int j = 0; j < n ; j++) {
+                if (matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m ; i++) {
+            for (int j = 0; j <n ; j++) {
+                if (row[i] || col[j] ){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
+
+        SerMatrixZero serMatrixZero = new SerMatrixZero();
+        serMatrixZero.setZeros(matrix);
+        for (int[] row : matrix){
+            for (int val : row){
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
